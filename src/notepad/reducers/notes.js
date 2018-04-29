0346d259570dae922be253902011683b5487878f
Sebastien Caietta <sebastien.caietta@theshopworks.com>
2018-04-29 23:13:53 +0100

    Add notes reducer
    This reducer will be responsible for manipulating the notes: Adding,
    deleting or updating them.

---
+++ src/notepad/reducers/notes.js
@@ -0,0 +1,39 @@
+const initialState = [
+    {
+        id: 1,
+        title: 'Read the docs',
+        content: 'Document myself with React-Router and ReactStrap'
+    },
+    {
+        id: 2,
+        title: 'First step',
+        content: 'Create notes list and editor to edit them'
+    },
+    {
+        id: 3,
+        title: 'Second step',
+        content: 'Navigate through the notes by clicking them and displaying their content in the editor'
+    },
+    {
+        id: 4,
+        title: 'Third step',
+        content: 'Update current note title and content when editing them'
+    },
+    {
+        id: 5,
+        title: 'Fourth step',
+        content: 'Create and delete notes'
+    },
+    {
+        id: 6,
+        title: 'Fifth step',
+        content: 'Use localStorage to get and save the notes'
+    },
+];
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
--- src/store.js
+++ src/store.js
@@ -1,3 +1,31 @@
+import {createStore, compose} from 'redux'
 import createHistory from 'history/createBrowserHistory'
+import {combineReducers} from 'redux'
+import notesReducer from './notepad/reducers/notes'
 
 export const history = createHistory();
+
+const initialState = {};
+const enhancers = [];
+
+if (process.env.NODE_ENV === 'development') {
+    const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
+
+    if (typeof devToolsExtension === 'function') {
+        enhancers.push(devToolsExtension())
+    }
+}
+
+const composedEnhancers = compose(...enhancers);
+
+const reducers = combineReducers({
+    notes: notesReducer,
+});
+
+const store = createStore(
+    reducers,
+    initialState,
+    composedEnhancers
+);
+
+export default store;
