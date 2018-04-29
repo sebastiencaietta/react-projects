8946c85e5b48387aa411d34263b11fbe370acdb7
Sebastien Caietta <sebastien.caietta@theshopworks.com>
2018-04-29 23:48:09 +0100

    Use localStorage to get/set the notes
    Persist the notes eveytime they are updated using localStorage.
    
    Use a generic save method that returns the current state, saving
    a lot of variable declarations in the reducer.

--- src/notepad/reducers/notes.js
+++ src/notepad/reducers/notes.js
@@ -1,51 +1,24 @@
 import {isLastNoteEmpty} from "../actions";
 
-const initialState = [
-    {
-        id: 1,
-        title: 'Read the docs',
-        content: 'Document myself with React-Router and ReactStrap'
-    },
-    {
-        id: 2,
-        title: 'First step',
-        content: 'Create notes list and editor to edit them'
-    },
-    {
-        id: 3,
-        title: 'Second step',
-        content: 'Navigate through the notes by clicking them and displaying their content in the editor'
-    },
-    {
-        id: 4,
-        title: 'Third step',
-        content: 'Update current note title and content when editing them'
-    },
-    {
-        id: 5,
-        title: 'Fourth step',
-        content: 'Create and delete notes'
-    },
-    {
-        id: 6,
-        title: 'Fifth step',
-        content: 'Use localStorage to get and save the notes'
-    },
-], newNote = () => {
-    let autoIncrement = parseInt(localStorage.getItem('notes_auto_increment'), 10) || 1;
-    localStorage.setItem('notes_auto_increment', autoIncrement + 1);
+const initialState = JSON.parse(localStorage.getItem('notes')) || [],
+    newNote = () => {
+        let autoIncrement = parseInt(localStorage.getItem('notes_auto_increment'), 10) || 1;
+        localStorage.setItem('notes_auto_increment', autoIncrement + 1);
 
-    return {
-        id: autoIncrement,
-        title: '',
-        content: '',
-    }
-};
+        return {
+            id: autoIncrement,
+            title: '',
+            content: '',
+        }
+    }, save = notes => {
+        localStorage.setItem('notes', JSON.stringify(notes));
+        return notes;
+    };
 
 export default (state = initialState, action) => {
     switch (action.type) {
         case 'SET_TITLE':
-            return state.map(note => {
+            return save(state.map(note => {
                 if (note.id !== action.id) {
                     return note;
                 }
@@ -53,9 +26,9 @@ export default (state = initialState, action) => {
                     ...note,
                     title: action.title,
                 }
-            });
+            }));
         case 'SET_CONTENT':
-            return state.map(note => {
+            return save(state.map(note => {
                 if (note.id !== action.id) {
                     return note;
                 }
@@ -64,14 +37,14 @@ export default (state = initialState, action) => {
                     ...note,
                     content: action.content,
                 }
-            });
+            }));
         case 'ADD_NOTE':
-            return isLastNoteEmpty(state) ? state : [
+            return isLastNoteEmpty(state) ? state : save([
                 newNote(),
                 ...state,
-            ];
+            ]);
         case 'DELETE_NOTE':
-            return state.filter(note => note.id !== action.id);
+            return save(state.filter(note => note.id !== action.id));
         default:
             return state;
     }
